f16498817d5fc273199bf4d566f2d5504746066c
Daniel Haag <danni.haag91@gmail.com>
2023-04-11 22:36:12 -0300

    Primeiro commit com EF Core configurado e primeira migration efetuada

---
+++ CursoEFCore_Aula1/Migrations/20230412012403_produtoInicial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoEFCore_Aula1.Migrations
+{
+    public partial class produtoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    ProdutoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.ProdutoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
---
+++ CursoEFCore_Aula1/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace CursoEFCore_Aula1
+{
+    internal class Program
+    {
+        //Entidade
+        public class Produto
+        {
+            public int ProdutoId { get; set; }
+            public string Nome { get; set;}
+            public decimal Preco { get; set; }
+        }
+
+        //Classe de contexto
+        public class AppDbContext : DbContext
+        {
+            //Mapeamento da entidade para a tabela
+            public DbSet<Produto> Produtos { get; set; }
+
+            //Provedor e string de conexão
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=DANIEL_HAAG\SQLEXPRESS;" +
+                    "Initial Catalog=Aula1DB;Integrated Security=True");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
