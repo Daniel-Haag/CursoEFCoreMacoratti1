ce16d06c991a3b32b495373fc14ab7686a6b8c53
Daniel Haag <danni.haag91@gmail.com>
2023-04-20 00:01:38 -0300

    Populando BD via migrations

---
+++ CursoEFCore_Aula1/Migrations/20230420025359_populaDB.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CursoEFCore_Aula1.Migrations
+{
+    public partial class populaDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Preco, Estoque)" +
+                "VALUES('Caneta', 4.99, 10)");
+
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Preco, Estoque)" +
+                "VALUES('Estojo', 6.50, 15)");
+
+            migrationBuilder.Sql("INSERT INTO Produtos(Nome, Preco, Estoque)" +
+                "VALUES('Borracha', 2.99, 25)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Produtos");
+        }
+    }
+}
