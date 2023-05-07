cb108bb101d0f7ddb0ab077a38b35f7fbfee07dd
Daniel Haag <danni.haag91@gmail.com>
2023-05-07 03:14:45 -0300

    Novos modelos de relacionamento

---
+++ CursoEFCore_Aula1/Models/Venda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoEFCore_Aula1.Models
+{
+    internal class Venda
+    {
+        public int VendaId { get; set; }
+        public Vendedor Vendedor { get; set; }
+        public Produto Produto { get; set; }
+
+    }
+}
---
+++ CursoEFCore_Aula1/Models/Vendedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoEFCore_Aula1.Models
+{
+    internal class Vendedor
+    {
+        public int VendedorId { get; set; }
+        public string Nome { get; set; }
+        public string Nivel { get; set; }
+        public decimal Salario { get; set; }
+        public string Setor { get; set; }
+    }
+}
